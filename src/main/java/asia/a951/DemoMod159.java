ff5d78346a87f813f75e8caefd8b3d5044f0613e
159L <3138711143@qq.com>
2025-04-12 13:02:05 +0800

    第二次修改

--- src/main/java/asia/a951/DemoMod159.java
+++ src/main/java/asia/a951/DemoMod159.java
@@ -1,5 +1,6 @@
 package asia.a951;
 
+import asia.a951.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class DemoMod159 implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello world!");
+		ModItems.registerItems();
 	}
 }
---
+++ src/main/java/asia/a951/registry/ModItems.java
@@ -0,0 +1,31 @@
+package asia.a951.registry;
+
+import asia.a951.DemoMod159;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+public class ModItems { //这里新建物品
+    public static final Item Normal_obs = registerItem("normal_obs",          //添加物品NormalOBS
+            new Item(new FabricItemSettings()));
+    public static final Item Loser_obs = registerItem("loser_obs",  //添加物品Loser OBS
+            new Item(new FabricItemSettings()));
+
+    public static Item registerItem(String name, Item item, RegistryKey<ItemGroup>... itemGroups){
+        Item registerItem = Registry.register(Registries.ITEM,new Identifier(DemoMod159.MOD_ID,name),item);
+        for (RegistryKey<ItemGroup> itemGroup:itemGroups){
+            ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(registerItem));
+
+        }
+        return registerItem;
+    }
+    public static  void registerItems(){
+        DemoMod159.LOGGER.debug("Registering mod items for" + DemoMod159.MOD_ID);
+    }
+}
