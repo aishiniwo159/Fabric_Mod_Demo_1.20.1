b8120a7ddf55b50ff4243412826a1101140f4598
159L <3138711143@qq.com>
2025-04-12 11:01:52 +0800

    第一次修改

---
+++ src/main/java/asia/a951/DemoMod159Client.java
@@ -0,0 +1,10 @@
+package asia.a951;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemoMod159Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/asia/a951/DemoMod159DataGenerator.java
@@ -0,0 +1,11 @@
+package asia.a951;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DemoMod159DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
